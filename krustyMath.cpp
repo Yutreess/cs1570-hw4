2693b25df336f61a501a0c6e343b31f29a29123e
Yutreeeeeeeeeeeeeesss <37532358+Yutreess@users.noreply.github.com>
2018-09-17 11:35:21 -0500

    Add files via upload

---
+++ krustyMath.cpp
@@ -0,0 +1,76 @@
+// Programmer:	Justus Peterson-Rhodes
+// Student ID:	jepfn5
+// Section:	A
+// Date: 	09/14/2018
+// File:	krustyMath.cpp
+// Description:
+//
+//
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  int option;
+  int num;
+  int base;
+  // Minimum and Maximum values permitted for num
+  const int MIN_NUM = 0;
+  const int MAX_NUM = 99999;
+  // Minimum and Maximum values for base
+  const int MIN_BASE = 10;
+  const int MAX_BASE = 2;
+  // Loop Controls for variable assigning attempts
+  int a = 0;
+  const int MAX_ATTEMPTS = 3;
+  do
+  {
+    a = 0;
+    cout << "-----OPTIONS-----" << endl;
+    cout << "1: Enter a non-negative Integer, x [5 digits or less]" << endl;
+    cout << "2: Enter an integer base n [2 <= n < 10]" << endl;
+    cout << "3: Verify x is a valid base n number" << endl;
+    cout << "4: Convert x to base 10" << endl;
+    cout << "5: Quit" << endl;
+    cin >> option;
+    switch (option)
+    {
+      case 1:
+        do
+        {
+          cout << "Enter a value for x: ";
+          cin >> num;
+          a++;
+        }
+        while (!(num >= MIN_NUM
+                && num <= MAX_NUM)
+                && a < MAX_ATTEMPTS);
+        cout << "x is now equal to " << num << endl;
+        break;
+      case 2:
+        do
+        {
+          cout << "Enter a value n: ";
+          cin >> base;
+          a++;
+        }
+        while (!(base >= MIN_BASE
+                && base <= MAX_BASE)
+                && a < MAX_ATTEMPTS);
+        cout << "n is now equal to " << base << endl;
+        break;
+      case 3:
+      // Verify x is a valid number of base n
+        break;
+      case 4:
+      // Convert x to base 10
+        break;
+    }
+  }
+  // TODO: Add proper signoff / goodbye statement
+  while (option != 5);
+  return 0;
+}
